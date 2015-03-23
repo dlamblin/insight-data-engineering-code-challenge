a08b29215540e136f62b3473ebe638f0658a7f02
Daniel Lamblin <dlamblin+github@gmail.com>
2015-03-22 20:26:13 -0400

    Match one-liner to java's handling of digits and empty strings

--- non-scalable_10min_oneliner1.pl
+++ non-scalable_10min_oneliner1.pl
@@ -1,1 +1,1 @@
-use warnings;use strict;undef $/;my %count=();$_=<>;foreach (split){s/\W//g;$count{lc()}++;};foreach my $word (sort keys(%count)){printf "%-16s%d\n", $word, $count{$word};}
+use warnings;use strict;undef $/;my %count=();while($_=<>){foreach (split){s/\W//g;s/^\d*$//;if(length){$count{lc()}++;}}};foreach my $word (sort keys(%count)){printf "%-15s\t%d\n", $word, $count{$word};}
