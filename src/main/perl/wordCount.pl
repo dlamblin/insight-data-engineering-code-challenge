93b603bb14c4c5c971f9ba427b467b8c0da61eb3
Daniel Lamblin <dlamblin+github@gmail.com>
2015-03-23 15:09:39 -0400

    PerlTidy

--- src/main/perl/wordCount.pl
+++ src/main/perl/wordCount.pl
@@ -9,18 +9,19 @@
 use warnings;
 use strict;
 
-my %count=();			# Establish a hash of words to their count
-while(<>) {			# Over each line from all files in the args
-	foreach (split){	# Split into whitespace delimited words
-		s/\W//g;	# Remove non-AlphaNumerics from words
-		s/^\d*$//;	# Remove if only digits in the words
-		if(length) {	# Count only non-empty words
-		$count{lc()}++;	# Lower-case the index to count the word
-		}
-	}
-}				# Surprise, we're done parsing input
-foreach my $word (sort keys(%count)) {	# Asciibetically order the words
-	printf "%-15s\t%d\n",	# Output the desired, "word tab count" view
-		$word, $count{$word};
+my %count = ();          # Establish a hash of words to their count
+while (<>) {             # Over each line from all files in the args
+    foreach (split) {    # Split into whitespace delimited words
+        s/\W//g;         # Remove non-AlphaNumerics from words
+        s/^\d*$//;       # Remove if only digits in the words
+        if (length) {    # Count only non-empty words
+            $count{ lc() }++;    # Lower-case the index to count the word
+        }
+    }
+}    # Surprise, we're done parsing input
+foreach my $word ( sort keys(%count) ) { # Asciibetically order the words
+    printf "%-15s\t%d\n",        # Output the desired, "word [tab]count" view
+      $word, $count{$word};
 }
+
 #This seems to time better than the Java... Less overhead maybe?
