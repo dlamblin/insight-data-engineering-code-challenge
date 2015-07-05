957f7fbabb3dfe32d06919d0f99602b34196a9e9
Daniel Lamblin <dlamblin+github@gmail.com>
2015-07-05 12:49:03 -0400

    Added quick perl oneliner, streams to stdout

--- non-scalable_10min_oneliner1.pl
+++
@@ -1,1 +0,0 @@
-use warnings;use strict;undef $/;my %count=();while($_=<>){foreach (split){s/\W//g;s/^\d*$//;if(length){$count{lc()}++;}}};foreach my $word (sort keys(%count)){printf "%-15s\t%d\n", $word, $count{$word};}
